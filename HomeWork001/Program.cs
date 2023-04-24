0497463a56abc3f5b8b4c9dbd9166eb1fda513df
Dmitriy <dpolozov86@gmail.com>
2023-04-24 22:39:24 +0300

    add HomeWork001 add the first task

---
+++ HomeWork001/Program.cs
@@ -0,0 +1,21 @@
+﻿// Урок 1. Знакомство с языком программирования С#
+// 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Insert first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Insert second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine("The first number greater than the second");
+// }
+// if (num1 == num2)
+// {
+//     Console.WriteLine("The first number is exactly the second");
+// }
+// else Console.WriteLine("The second number greater than the first");
+
+
+// 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
