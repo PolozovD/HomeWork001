96abc3436f12e255e4e1049ab9b384916ff83dea
Dmitriy <dpolozov86@gmail.com>
2023-04-25 21:18:31 +0300

    add Task 3

--- HomeWork001/Program.cs
+++ HomeWork001/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Урок 1. Знакомство с языком программирования С#
 // 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-// Console.WriteLine("Insert first number: ");
+// Console.WriteLine("Enter first number: ");
 // int num1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Insert second number: ");
+// Console.WriteLine("Enter second number: ");
 // int num2 = Convert.ToInt32(Console.ReadLine());
 
 // if (num1 > num2)
@@ -19,22 +19,33 @@
 
 // 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine("Insert first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Insert second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Insert second number: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter second number: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
 
-int max = num1;
+// int max = num1;
 
-if(num1 > max) max = num1;
-if(num2 > max) max = num2;
-if(num3 > max) max = num3;
+// if(num1 > max) max = num1;
+// if(num2 > max) max = num2;
+// if(num3 > max) max = num3;
 
-Console.WriteLine($"max = {max}");
+// Console.WriteLine($"max = {max}");
+
 // или
+
 // Console.Write("max = ");
 // Console.WriteLine(max);
 
 // 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.WriteLine("The number is even");
+}
+else Console.WriteLine("This is an odd number");
