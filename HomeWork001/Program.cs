a60e5b06810c8a2d8a3e959329a90a5996a1ce9e
Dmitriy <dpolozov86@gmail.com>
2023-04-25 21:02:46 +0300

    add Second task

--- HomeWork001/Program.cs
+++ HomeWork001/Program.cs
@@ -19,3 +19,22 @@
 
 // 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+Console.WriteLine("Insert first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert second number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if(num1 > max) max = num1;
+if(num2 > max) max = num2;
+if(num3 > max) max = num3;
+
+Console.WriteLine($"max = {max}");
+// или
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+// 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
